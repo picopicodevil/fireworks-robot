6a044b8a89899059581b8a83dab5540cca86ac8e
picopicodevil <picopicodevil@gmail.com>
2020-10-18 20:48:03 +0900

    Fix: IRセンサのカウントを修正

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <chrono>
 #include <cstring>
 
-#define TURN_INTERVAL_TIME 7.0f
+#define TURN_INTERVAL_TIME 2.0f
 
 int move2value(char *move)
 {
@@ -32,7 +32,6 @@ void setRgbLed(DigitalOut *rgb_led, int *color)
     }
 }
 
-
 // main() runs in its own thread in the OS
 int main()
 {
@@ -40,97 +39,120 @@ int main()
     char move[MOVE_LENGTH][6] = REPLACE_MOVE;
     int color[COLOR_LENGTH][3] = REPLACE_COLOR;
 
+    BufferedSerial pc(USBTX, USBRX);
+
     IrRemoteControl controller(PF_0);
     TB6612 tb6612(PA_5, PA_4, PA_8, PA_7, PB_0, PB_1, PA_6);
     LineTrace line_trace(PA_0, PA_1, PA_3);
 
-    DigitalOut led1(PB_6);
-    DigitalOut led2(PB_7);
+    BusOut led(PB_6, PB_7);
 
     DigitalOut rgb_led[] = {
-        DigitalOut(PB_5),
-        DigitalOut(PB_3),
-        DigitalOut(PB_4),
+        DigitalOut(PB_5, 1),
+        DigitalOut(PB_3, 1),
+        DigitalOut(PB_4, 1),
     };
 
-    for (int i = 0; i < 10; i++)
-    {
-        led1 = !led1;
-        led2 = !led2;
-        ThisThread::sleep_for(100ms);
-    }
+    // rgb_led[0] = 1;
+    // rgb_led[1] = 0;
+    // rgb_led[2] = 1;
 
-    while (controller.get_reader_code() <= 5)
-    {
-        led1 = !led1;
-        led2 = !led2;
-        ThisThread::sleep_for(10ms);
-    }
+    // int i = 0;
+    // while(1){
+    //     led = i++;
+    //     ThisThread::sleep_for(1s);
+    // }
+
+    // while (controller.get_reader_code() >= 5)
+    // {
+    //     led1 = !led1;
+    //     led2 = !led2;
+    //     ThisThread::sleep_for(100ms);
+    // }
 
     controller.reset_reader_code();
 
     tb6612.standby(1);
 
-
-
     // 各ターンの動作
-    for (int i = 0; i < MOVE_LENGTH; i++)
+    for (int i = 0; (i < MOVE_LENGTH) || (i < COLOR_LENGTH); i++)
     {
         Timer t;
         t.start();
 
-        setRgbLed(rgb_led, color[i]);
+        if (i < COLOR_LENGTH)
+            setRgbLed(rgb_led, color[i]);
 
         Motor motor[2];
 
-        if (controller.get_reader_code() <= 5)
+        led = i;
+
+        if (controller.get_reader_code() >= 5)
             break;
 
         // @TODO ここで直進、後進、右折、左折のコードを記述する
         // int result = line_trace.read();
 
-        int move_value = move2value(move[i]);
-
-        switch (move_value)
+        if (i < MOVE_LENGTH)
         {
-        case 2:
-            // // TurnRight
-            motor[0].set_state(State::CCW);
-            motor[0].set_duty_cycle(0.50f);
-            motor[1].set_state(State::CCW);
-            motor[1].set_duty_cycle(0.20f);
+            int move_value = move2value(move[i]);
 
-            tb6612.set(motor[0], 0);
-            tb6612.set(motor[1], 1);
+            switch (move_value)
+            {
+            case 0:
+                // Stay
+                while (1)
+                {
+                    if (controller.get_reader_code() >= 5)
+                        break;
 
-            ThisThread::sleep_for(700ms);
-            break;
+                    if (t.read() > TURN_INTERVAL_TIME)
+                        break;
 
-        case 4:
-            // turn left
-            motor[0].set_state(State::Brake);
-            motor[0].set_duty_cycle(0.00f);
-            motor[1].set_state(State::CW);
-            motor[1].set_duty_cycle(0.50f);
+                    ThisThread::sleep_for(100ms);
+                }
+                break;
+            case 2:
+                // // TurnRight
+                motor[0].set_state(State::CCW);
+                motor[0].set_duty_cycle(0.50f);
+                motor[1].set_state(State::CCW);
+                motor[1].set_duty_cycle(0.20f);
 
-            tb6612.set(motor[0], 0);
-            tb6612.set(motor[1], 1);
+                tb6612.set(motor[0], 0);
+                tb6612.set(motor[1], 1);
 
-            ThisThread::sleep_for(700ms);
-            break;
+                ThisThread::sleep_for(700ms);
+                break;
 
-        default:
-            break;
+            case 4:
+                // turn left
+                motor[0].set_state(State::CW);
+                motor[0].set_duty_cycle(0.020f);
+                motor[1].set_state(State::CW);
+                motor[1].set_duty_cycle(0.50f);
+
+                tb6612.set(motor[0], 0);
+                tb6612.set(motor[1], 1);
+
+                ThisThread::sleep_for(700ms);
+                break;
+
+            default:
+                break;
+            }
         }
 
         while (true)
         {
-            if (controller.get_reader_code() <= 5)
+            if (controller.get_reader_code() >= 5)
                 break;
+
+            printf("%f", t.read());
 
             if (t.read() > TURN_INTERVAL_TIME)
                 break;
-            
+
             motor[0].set_state(line_trace.get_left_state());
             motor[0].set_duty_cycle(line_trace.get_left_duty_cycle());
             motor[1].set_state(line_trace.get_right_state());
@@ -139,15 +161,14 @@ int main()
             tb6612.set(motor[0], 0);
             tb6612.set(motor[1], 1);
 
-            led1 = !led1;
-            led2 = !led2;
             ThisThread::sleep_for(10ms);
         }
 
         t.stop();
-        t.start();
+        t.reset();
     }
 
     tb6612.standby(0);
+    rgb_led[0] = rgb_led[1] = rgb_led[2] = 1;
     sleep();
 }
