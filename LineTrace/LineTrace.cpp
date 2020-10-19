72fa20f8e39bd087a656843da53f3780194c28f4
picopicodevil <picopicodevil@gmail.com>
2020-10-19 13:52:56 +0900

    Update: ライン上にすべてのセンサが来た時の条件文を変更

--- LineTrace/LineTrace.cpp
+++ LineTrace/LineTrace.cpp
@@ -15,8 +15,7 @@ int LineTrace::read()
     _place[TracePosition::Right] = get_place(_right);
 
     if ((_place[TracePosition::Center] == Place::Line) &&
-        (_place[TracePosition::Left] == _place[TracePosition::Center]) &&
-        (_place[TracePosition::Center] == _place[TracePosition::Left]) &&
+        (_place[TracePosition::Left] == Place::Line) &&
         (_place[TracePosition::Right] == Place::Line))
     {
         // // Up
